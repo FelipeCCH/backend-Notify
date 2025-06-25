e1a4678d14ad0211983eb9b468e8091553912522
Felipe Campos <camposfelipecch@gmail.com>
2025-06-25 00:28:36 -0600

    Fix: Correccion de errores para el deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ cd /var/www/html
 php artisan config:clear
 php artisan config:cache
 php artisan route:cache
-php artisan view:cache
 php artisan migrate --force
 
 # Ejecuta schedule:run cada minuto en segundo plano
